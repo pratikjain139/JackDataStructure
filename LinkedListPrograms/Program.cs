036c7813c44ed2169c17da602ecffa99047a1584
pratikjain139 <pratikjain139@gmail.com>
2017-11-28 10:42:07 +0530

    Added linkedlist and node class

--- LinkedListPrograms/Program.cs
+++ LinkedListPrograms/Program.cs
@@ -6,10 +6,51 @@ using System.Threading.Tasks;
 
 namespace LinkedListPrograms
 {
+    public class LinkedList
+    {
+        public Node head;
+        public LinkedList(Node h)
+        {
+            head = h;
+        }
+
+        public void printAllNodes() 
+        {
+            Node currentNode= head;
+            do
+            {
+                Console.WriteLine(currentNode.data.ToString());
+                currentNode = currentNode.next;
+            }
+            while (currentNode != null);
+            Console.ReadLine();
+        }
+    }
+
+    public class Node
+    {
+        public int data;
+        public Node next;
+        public Node previous;
+        public Node(int d)
+        {
+            data = d;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Node firstNode= new Node(1);
+            LinkedList linkLst = new LinkedList(firstNode);
+            Node secondNode = new Node(2);
+            Node thirdNode = new Node(3);
+            firstNode.next = secondNode;
+
+            secondNode.next = thirdNode;
+            secondNode.previous = firstNode;
+            thirdNode.previous = secondNode;
+            linkLst.printAllNodes();
         }
     }
 }
